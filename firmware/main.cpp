635927d36bfa3a17e3b930c04f3483516bd43749
Nicolle Laranjeira <nicolle.laranjeira02@gmail.com>
2026-09-10 16:19:26 -0300

    teste de pulsada hidranomic

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,25 +1,13 @@
 #include <Arduino.h>
 
-const int PINO = 2; // D2 (também o LED onboard na maioria das placas ESP32)
-unsigned long contador = 0;
+const int PINO = 15;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(PINO, OUTPUT);
-  digitalWrite(PINO, LOW);
-  Serial.println("Gerador de pulsos iniciado no D2...");
+  pinMode(PINO, INPUT_PULLUP);
 }
 
 void loop() {
-  // Gera o pulso (50 ms em HIGH)
-  digitalWrite(PINO, HIGH);
-  delay(50);
-  digitalWrite(PINO, LOW);
-
-  contador++;
-  Serial.print("Pulso: ");
-  Serial.println(contador);
-
-  // Intervalo entre pulsos (ex: 1 segundo)
-  delay(1000);
+  Serial.println(digitalRead(PINO));
+  delay(200);
 }
