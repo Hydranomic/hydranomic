6fc28557a1c92d86b61ed5cf1149f1d34efa0724
Nicolle Laranjeira <nicolle.laranjeira02@gmail.com>
2026-09-17 13:13:21 -0300

    conexão da esp com o broker da weg + lógica de contagem de litros

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,86 +1,123 @@
-#include <Arduino.h>
+#include <WiFi.h>
 
-const int PINO = 2;
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
 
-// Conversão do hidrômetro
-const float LITROS_POR_PULSO = 0.10;
+// Conexão esp com sensor
+int SENSOR = 15;
 
-// Intervalo para calcular a vazão
-const unsigned long INTERVALO = 10000; // 10 segundos
+int leituraHidrometro;
+int estadoAnterior = HIGH;
 
-unsigned long contador = 0;
-unsigned long contadorAnterior = 0;
+int count = 0;
+int id = 1;
 
-unsigned long tempoAnterior = 0;
 
-float litros = 0;
-float vazao = 0;
+// Conexão esp e broker
+
+
+const char* WIFI_SSID = "Nicolle";
+const char* WIFI_PASSWORD = "Nicolle13";
+
+/////////////////////////////////////////////////////////////
+const char* MQTT_SERVER = "broker.app.wnology.io";
+const char* MQTT_ACCESS_KEY = "aa6fa6d5-f77d-46da-b4e4-f23bf995d04f";
+const char* MQTT_ACCESS_SECRET = "77547f8f4f0d805edeb1982eadf46251d6bf0bf8b565b9e3c7fd92746459807b";
+const char* DEVICE_ID = "6aa2e97be257647a5e068ae4";
+const char* MQTT_TOPIC = "wnology/6aa2e97be257647a5e068ae4/state";
+const int MQTT_PORT = 1883;
+const int PUBLISH_INTERVAL = 300000;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+char attributes[200];
+
+void connectWiFi();
+void connectMQTT();
+void publishData();
 
 void setup() {
+  pinMode(SENSOR, INPUT);
+  Serial.begin(115200);
 
-    Serial.begin(115200);
+  connectWiFi();
+  connectMQTT();
 
-    pinMode(PINO, OUTPUT);
-    digitalWrite(PINO, LOW);
-
-    tempoAnterior = millis();
-
-    Serial.println("Gerador de pulsos iniciado!");
 }
 
 void loop() {
 
-    // =========================
-    // GERA UM PULSO
-    // =========================
+    leituraHidrometro = digitalRead(SENSOR);
 
-    digitalWrite(PINO, HIGH);
-    delay(50);
+    if (leituraHidrometro == LOW && estadoAnterior == HIGH) {
 
-    digitalWrite(PINO, LOW);
+    count++;
 
-    contador++;
+    Serial.print("Pulso: ");
+    Serial.println(count);
 
-    Serial.print("Pulso total: ");
-    Serial.println(contador);
+    publishData();
+}
 
-    // Espera aproximadamente 1 segundo
-    delay(1000);
+estadoAnterior = leituraHidrometro;
 
+}
 
-    // =========================
-    // CALCULA A VAZAO
-    // =========================
-
-    if (millis() - tempoAnterior >= INTERVALO) {
-
-        // Quantidade de pulsos ocorridos
-        unsigned long pulsosIntervalo =
-            contador - contadorAnterior;
-
-        // Converte pulsos para litros
-        litros = pulsosIntervalo * LITROS_POR_PULSO;
-
-        // Calcula vazão em L/min
-        vazao = (litros / 10.0) * 60.0;
-
-        Serial.println("-------------------------");
-
-        Serial.print("Pulsos no intervalo: ");
-        Serial.println(pulsosIntervalo);
-
-        Serial.print("Litros no intervalo: ");
-        Serial.println(litros);
-
-        Serial.print("Vazao: ");
-        Serial.print(vazao);
-        Serial.println(" L/min");
-
-        Serial.println("-------------------------");
-
-
-        // Atualiza os valores para o próximo intervalo
-        contadorAnterior = contador;
-        tempoAnterior = millis();
+void connectMQTT() {
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT server...");
+    if (client.connect(DEVICE_ID, MQTT_ACCESS_KEY, MQTT_ACCESS_SECRET)) {
+      Serial.println("Connected to WEGNOLOGY platform");
+      Serial.println("-------------"); 
+    } else {
+      Serial.print("Failed to connect to WEGNOLOGY platform, error code: ");
+      Serial.print(client.state());
+      delay(2000);
     }
+  }
+}
+
+void connectWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi network");
+  Serial.println("-------------"); 
+}
+
+
+void publishData() {
+  
+  DynamicJsonDocument doc(200);
+  doc["data"]["id"] = id;
+  doc["data"]["Pulsos"] = count;
+
+  String payload;
+  serializeJson(doc, payload);
+  payload.toCharArray(attributes, 200);
+  Serial.println(payload);
+
+  if (client.connected()) {
+      client.publish(MQTT_TOPIC, attributes);
+    }
+}
+
+void call_pub() {
+  if (!client.connected()) {
+    Serial.println("Reconnecting to WEGNOLOGY platform");
+    connectMQTT();
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Reconnecting to WiFi");
+    connectWiFi();
+  }
+
+  client.loop();
+  publishData();
+  Serial.println("-------------");    
 }
