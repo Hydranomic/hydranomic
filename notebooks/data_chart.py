1a9f8b0811e67e22b8a6298c369f319d1b65993b
kurachix <matheuskurachi6@gmail.com>
2026-09-24 15:21:15 -0300

    Add input/output file definitions in data_chart.py

--- notebooks/data_chart.py
+++ notebooks/data_chart.py
@@ -5,3 +5,7 @@ import json
 # Guarda o ano atual, para garantir a seleção necessária dos dados.
 verificar_data = datetime.now()
 ano_atual = verificar_data.year
+
+# Define os arquivos de entrada / saída.
+arquivo_entrada = "hydranomic/workflows/conta_povoada.json"
+arquivo_saida = "hydranomic/workflows/grafico_anual.json"
