71ce68a477f1786dbee8a0da4894008c8402dc07
kurachix <matheuskurachi6@gmail.com>
2026-09-24 10:09:31 -0300

    Add comments to improve code readability in machinelearning.py

--- notebooks/machinelearning.py
+++ notebooks/machinelearning.py
@@ -1,6 +1,5 @@
-import json
+import json # Biblioteca utilizada para leitura de arquivos JSON.
 
 with open("hydranomic/workflows/conta.json", "r", encoding="utf-8") as info_contas:
-    dados = json.load(info_contas)
+    dados = json.load(info_contas) # Função utilizada para leitura do arquivo JSON e armazenamento na váriavel.
 
-print(dados)
