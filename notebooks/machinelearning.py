320f8199d15b379fceff8a24b2314b4a1133d095
kurachix <matheuskurachi6@gmail.com>
2026-09-24 10:18:47 -0300

    Add imports for pandas and RandomForestRegressor in machinelearning.py

--- notebooks/machinelearning.py
+++ notebooks/machinelearning.py
@@ -1,4 +1,6 @@
 import json # Biblioteca utilizada para leitura de arquivos JSON.
+import pandas as pd # Transforma o JSON bruto em tabelas organizadas.
+from sklearn.ensemble import RandomForestRegressor # Modelo de ML Regressivo, utilizado para as previsões.
 
 with open("hydranomic/workflows/conta.json", "r", encoding="utf-8") as info_contas:
     dados = json.load(info_contas) # Função utilizada para leitura do arquivo JSON e armazenamento na váriavel.
