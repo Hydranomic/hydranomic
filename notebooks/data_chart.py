5118f113fe148fd6861276641772fea91e85c29a
kurachix <matheuskurachi6@gmail.com>
2026-09-24 15:37:31 -0300

    Refactor: add JSON data loading logic in data_chart.py

--- notebooks/data_chart.py
+++ notebooks/data_chart.py
@@ -9,3 +9,10 @@ ano_atual = verificar_data.year
 # Define os arquivos de entrada / saída.
 arquivo_entrada = "hydranomic/workflows/conta_povoada.json"
 arquivo_saida = "hydranomic/workflows/grafico_anual.json"
+
+# Abre e Armazena os dados dos JSON's.
+with open(arquivo_entrada, "r", encoding="utf-8") as dados:
+    data = json.load(dados)
+
+with open(arquivo_saida, "r", encoding="utf-8") as dados:
+    data_novo = json.load(dados)
