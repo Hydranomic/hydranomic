bcb7868d25cafd2713ff82a929f272f01e2057b3
kurachix <matheuskurachi6@gmail.com>
2026-09-24 14:42:03 -0300

    Fix comment formatting and ensure newline at end of file in data.py

--- notebooks/data.py
+++ notebooks/data.py
@@ -1,3 +1,7 @@
-# É utilizado para recuperar o ano atual proveniente do servidor
+# É utilizado para recuperar o ano atual proveniente do servidor.
 from datetime import datetime
 import json
+
+# Guarda o ano atual, para garantir a seleção necessária dos dados.
+verificar_data = datetime.now()
+ano_atual = verificar_data.year
