d66114916b353b17596dd6707bdc95164521c052
kurachix <matheuskurachi6@gmail.com>
2026-09-24 10:51:37 -0300

    Update data source in machinelearning.py to use conta_povoada.json; create conta_povoada.json with detailed consumer data

--- notebooks/machinelearning.py
+++ notebooks/machinelearning.py
@@ -2,7 +2,7 @@ import json # Biblioteca utilizada para leitura de arquivos JSON.
 import pandas as pd # Transforma o JSON bruto em tabelas organizadas.
 from sklearn.ensemble import RandomForestRegressor # Modelo de ML Regressivo, utilizado para as previsões.
 
-with open("hydranomic/workflows/conta.json", "r", encoding="utf-8") as info_contas:
+with open("hydranomic/workflows/conta_povoada.json", "r", encoding="utf-8") as info_contas:
     dados = json.load(info_contas) # Função utilizada para leitura do arquivo JSON e armazenamento na váriavel.
 
 # Converte o JSON bruto no DataFrame do Pandas.
