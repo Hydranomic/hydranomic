aaa7c71cacf7101e8f11987c5ab849fce6f9f827
kurachix <matheuskurachi6@gmail.com>
2026-09-24 10:42:53 -0300

    Fix DataFrame year extraction logic in machinelearning.py

--- notebooks/machinelearning.py
+++ notebooks/machinelearning.py
@@ -13,6 +13,6 @@ dataframe = dataframe.sort_values(by=["unidade_consumidora", "id"]).reset_index(
 
 # De acordo com as limitações matemáticas do modelo, é necessário extrair as datas para evitar o ValueError.
 # Fluxo - Separa o Mês, procura na parte ID, procura o espaço -, pega o item do vetor 1, tranforma o str em INT.
+dataframe["ano"] = dataframe["id"].str.split("-").str[0].astype(int)
 dataframe["mes"] = dataframe["id"].str.split("-").str[1].astype(int)
 
-print(dataframe)
